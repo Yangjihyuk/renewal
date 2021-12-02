d1e2de6dd9864dc672029cf77501f463a3989f88
Yangjihyuk <a9321705@naver.com>
2021-12-03 04:43:29 +0900

    20211203 re

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,26 @@
-// main-visual-swiper
-var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+// main-slick-slide
+$('.main-slide').slick({
+slidesToShow: 1,
+slidesToScroll: 1,
+dots:true,
+arrows: false,
+fade: true,
+autoplay:3000,
+Infinity,
+customPaging : function(slider, i) {
+        var title = $(slider.$slides[i].innerHTML).find('div[data-title]').data('title');
+        return '<a class="pager__item"> '+title+' </a>';
     },
-    loop: true,
-    effect:'fade',
-
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
-      },
-    }
 });
 
-
-  
-
-
-
-  
+// main-swiper-mobie
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
